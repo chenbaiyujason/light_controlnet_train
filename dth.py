e39e51c961c7eecf84fadf22d521625e4c750c72
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2023-04-17 11:24:10 +0800

    Update dth.py

--- dth.py
+++ dth.py
@@ -11,7 +11,8 @@ from PIL import Image
 # save_dir = "/mnt/disks/persist/datasets/grayscale_image_aesthetic_3M"
 # Path(save_dir).mkdir(parents=True, exist_ok=True)
 
-cache_dir = "/mnt/disks/data/cache/deimg"
+cache_dir = "/mnt/disks/data/cache/consdeimg"
+Path(cache_dir).mkdir(parents=True, exist_ok=True)
 odatapath="/mnt/disks/data/consdata/consdeimg/"
 dataset = load_dataset(odatapath, cache_dir=cache_dir )
 # dataset.save_to_disk(save_dir, max_shard_size="1GB")
