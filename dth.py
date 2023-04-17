738267c20b5c094cb91453063fd84b5f87d3dce7
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2023-04-17 11:30:22 +0800

    Update dth.py

--- dth.py
+++ dth.py
@@ -15,6 +15,6 @@ from PIL import Image
 cache_dir = "/mnt/disks/data/cache/consdeimg"
 Path(cache_dir).mkdir(parents=True, exist_ok=True)
 odatapath="/mnt/disks/data/consdata/consdeimg/train"
-dataset = load_from_disk(odatapath, cache_dir )
+dataset = load_from_disk(odatapath )
 # dataset.save_to_disk(save_dir, max_shard_size="1GB")
 dataset.push_to_hub('ioclab/light', private=True, max_shard_size="1GB")
