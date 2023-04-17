1e7bc1c11886de6ceedb58a898f608cbfeab4aea
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2023-04-17 13:32:43 +0800

    1

--- dth.py
+++ dth.py
@@ -17,4 +17,4 @@ Path(cache_dir).mkdir(parents=True, exist_ok=True)
 odatapath="/mnt/disks/data/consdata/consdeimg/train"
 dataset = load_from_disk(odatapath )
 # dataset.save_to_disk(save_dir, max_shard_size="1GB")
-dataset.push_to_hub('ioclab/light', private=True, max_shard_size="1GB")
+dataset.push_to_hub('ioclab/light', private=True, max_shard_size="10GB")
