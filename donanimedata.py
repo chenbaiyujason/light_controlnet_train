0a5b65e6fb700ce2d40efff0dfefb305a40649c6
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2023-04-24 11:45:34 +0800

    Update donanimedata.py

--- donanimedata.py
+++ donanimedata.py
@@ -1,74 +1,74 @@
 # -*- coding: utf-8 -*-
 import os
 
-from PIL import Image, ImageFilter
-import matplotlib.pyplot as plt
-from PIL import Image, ImageEnhance, ImageFilter,ImageOps
-import numpy as np
-import cv2
+# from PIL import Image, ImageFilter
+# import matplotlib.pyplot as plt
+# from PIL import Image, ImageEnhance, ImageFilter,ImageOps
+# import numpy as np
+# import cv2
 import datasets
-from danbooru2022 import DanbooruDataset
-from datasets import load_dataset
+# from danbooru2022 import DanbooruDataset
+# from datasets import load_dataset
 from datasets import load_dataset_builder,load_from_disk
-from pathlib import Path
-import wandb
+# from pathlib import Path
+# import wandb
 # 更改此路径为你希望将数据集下载到的目录
 custom_cache_dir = "/mnt/disks/hfcache/data2"
 # Path(custom_cache_dir).mkdir(parents=True, exist_ok=True)
 # os.environ["HF_DATASETS_CACHE"] = custom_cache_dir
-import random
-def imgprocess(img):
-    rand_num = random.uniform(-0.3, 1)
-    rand_num = round(rand_num, 2)
-    # 打开原始图像
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-
-    img = img.convert('L')
-    # img = ImageOps.equalize(img, mask=None)
-    img = img.resize((1536, 1536), resample=Image.BILINEAR)
-    enhancer = ImageEnhance.Contrast(img)
-    img_contrast = enhancer.enhance(1.3)
-    img = img_contrast
-
-    # 使用高斯模糊去除一部分细节
-    img = img.filter(ImageFilter.GaussianBlur(radius=5))
-    img = img.filter(ImageFilter.BoxBlur(radius=15))
-    img = img.filter(ImageFilter.GaussianBlur(radius=10))
-
-    # 使用中值滤波器去除更多的细节
-    img = img.filter(ImageFilter.MedianFilter(size=3))
-    img = img.filter(ImageFilter.SMOOTH)
-    # 使用边缘增强滤波器保留光影和色块的模糊关系
-
-    img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=1))
-
-    img_array = np.array(img)
-
-    img = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
-
-    # 将图像转换为灰度图像
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # 对图像进行自适应直方图均衡化
-    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
-    cl_img = clahe.apply(gray)
-
-    # 将图像转换为 Pillow 格式，并显示处理后的图像
-    img = Image.fromarray(cl_img)
-    img = img.resize((img.size[0] // 16, img.size[1] // 16), resample=Image.BOX)
-    img = img.resize((img.size[0] * 16, img.size[1] * 16), resample=Image.NEAREST)
-    img = img.filter(ImageFilter.GaussianBlur(radius=10))
-    enhancer = ImageEnhance.Contrast(img)
-    img_contrast = enhancer.enhance(1.2 + rand_num * 0.15)
-    img = img_contrast
-    img = img.resize((512, 512), resample=Image.BILINEAR)
-
-    return img
-
-def transforms(examples):
-    examples["conditioning_image"] = [imgprocess(image) for image in examples["image"]]
-    return examples
+# import random
+# def imgprocess(img):
+#     rand_num = random.uniform(-0.3, 1)
+#     rand_num = round(rand_num, 2)
+#     # 打开原始图像
+#     if img.mode != 'RGB':
+#         img = img.convert('RGB')
+#
+#     img = img.convert('L')
+#     # img = ImageOps.equalize(img, mask=None)
+#     img = img.resize((1536, 1536), resample=Image.BILINEAR)
+#     enhancer = ImageEnhance.Contrast(img)
+#     img_contrast = enhancer.enhance(1.3)
+#     img = img_contrast
+#
+#     # 使用高斯模糊去除一部分细节
+#     img = img.filter(ImageFilter.GaussianBlur(radius=5))
+#     img = img.filter(ImageFilter.BoxBlur(radius=15))
+#     img = img.filter(ImageFilter.GaussianBlur(radius=10))
+#
+#     # 使用中值滤波器去除更多的细节
+#     img = img.filter(ImageFilter.MedianFilter(size=3))
+#     img = img.filter(ImageFilter.SMOOTH)
+#     # 使用边缘增强滤波器保留光影和色块的模糊关系
+#
+#     img = img.filter(ImageFilter.UnsharpMask(radius=2, percent=150, threshold=1))
+#
+#     img_array = np.array(img)
+#
+#     img = cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR)
+#
+#     # 将图像转换为灰度图像
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+#     # 对图像进行自适应直方图均衡化
+#     clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(8,8))
+#     cl_img = clahe.apply(gray)
+#
+#     # 将图像转换为 Pillow 格式，并显示处理后的图像
+#     img = Image.fromarray(cl_img)
+#     img = img.resize((img.size[0] // 16, img.size[1] // 16), resample=Image.BOX)
+#     img = img.resize((img.size[0] * 16, img.size[1] * 16), resample=Image.NEAREST)
+#     img = img.filter(ImageFilter.GaussianBlur(radius=10))
+#     enhancer = ImageEnhance.Contrast(img)
+#     img_contrast = enhancer.enhance(1.2 + rand_num * 0.15)
+#     img = img_contrast
+#     img = img.resize((512, 512), resample=Image.BILINEAR)
+#
+#     return img
+#
+# def transforms(examples):
+#     examples["conditioning_image"] = [imgprocess(image) for image in examples["image"]]
+#     return examples
 # # 选择子集，将 '0-sfw' 更改为 '1-full' 或 '2-tags' 以下载其他子集
 # builder = DanbooruDataset(config_name='0-sfw')
 #
