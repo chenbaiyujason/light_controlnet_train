cc35264e898e35e7eae7754462a0f8e29f78ed7d
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2023-04-25 12:33:07 +0800

    Update pdanime.py

--- pdanime.py
+++ pdanime.py
@@ -100,5 +100,6 @@ print(dataset.column_names)
 print(dataset.num_columns)
 print(dataset.num_rows)
 # dataset.save_to_disk(odatapath)
-dataset.rename_dataset('light1000')
+dataset=load_from_disk(testdatapath)
+
 dataset.push_to_hub('ioclab/lighttestout', private=False, max_shard_size="1GB")
